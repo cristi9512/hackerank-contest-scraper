0799b77a599677cf001802762ab9d49338081760
Cristi Nica <nicacristi20@gmail.com>
2021-04-05 13:51:58 +0300

    Initial commit

---
+++ hackerrankAPI.js
@@ -0,0 +1,43 @@
+const fetch = require("node-fetch");
+
+const getSubmission = (cookie, submissionId, contestSlug) => {
+  configs.headers.cookie = cookie;
+  return fetch(
+    `https://www.hackerrank.com/rest/contests/${contestSlug}/submissions/${submissionId}`,
+    configs
+  );
+};
+
+const getAllSubmissionsForContest = (cookie, contestSlug) => {
+  configs.headers.cookie = cookie;
+  return fetch(
+    `https://www.hackerrank.com/rest/contests/${contestSlug}/judge_submissions?limit=100`,
+    configs
+  );
+};
+
+const configs = {
+  headers: {
+    accept:
+      "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+    "accept-language": "ro-RO,ro;q=0.9,en-US;q=0.8,en;q=0.7",
+    "cache-control": "max-age=0",
+    "if-none-match": 'W/"2f7d6cd78ee522f371a68bfa214a5ee4"',
+    "sec-ch-ua":
+      '"Google Chrome";v="89", "Chromium";v="89", ";Not A Brand";v="99"',
+    "sec-ch-ua-mobile": "?0",
+    "sec-fetch-dest": "document",
+    "sec-fetch-mode": "navigate",
+    "sec-fetch-site": "none",
+    "sec-fetch-user": "?1",
+    "upgrade-insecure-requests": "1",
+    cookie: undefined,
+  },
+  referrerPolicy: "strict-origin-when-cross-origin",
+  body: null,
+  method: "GET",
+  mode: "cors",
+};
+
+exports.getSubmission = getSubmission;
+exports.getAllSubmissionsForContest = getAllSubmissionsForContest;
---
+++ scraper.js
@@ -0,0 +1,99 @@
+const fetch = require("node-fetch");
+const R = require("ramda");
+const util = require("util");
+const fs = require("fs");
+const converter = require("json-2-csv");
+
+const {
+  getAllSubmissionsForContest,
+  getSubmission,
+} = require("./hackerrankAPI");
+
+const groupStudentsSubmissionsById = R.groupBy((student) => student.hacker_id);
+
+const groupSubmissionsByChallenge = R.groupBy(
+  (submission) => submission.challenge_id
+);
+
+const snooze = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+const main = () => {
+  const contestSlug = process.env.CONTEST_SLUG;
+  const cookie = process.env.COOKIE;
+
+  if (!contestSlug || !cookie) {
+    console.error("Process failed: Missing contest slug or cookie param");
+    process.exit(-1);
+  }
+
+  getAllSubmissionsForContest(cookie, contestSlug)
+    .then((res) => res.clone().json())
+    .then(async (jsonResponse) => {
+      const grouped = groupStudentsSubmissionsById(jsonResponse.models);
+      const aux = R.map(groupSubmissionsByChallenge, grouped);
+      /*
+      {
+        studentID:{
+          challengeId: [submissionId]
+        }
+      }
+      */
+
+      const studentIds = [];
+      const responses = [];
+
+      Object.keys(aux).forEach((studentId) => {
+        studentIds.push(studentId);
+      });
+
+      for (let j = 0; j < studentIds.length; j++) {
+        const studentSubmitedChallenges = aux[studentIds[j]];
+        const submissionsIds = [];
+        Object.keys(studentSubmitedChallenges).forEach((challengeId) => {
+          submissionsIds.push({
+            submissionId: studentSubmitedChallenges[challengeId][0].id,
+            challengeName:
+              studentSubmitedChallenges[challengeId][0].challenge.name,
+            score: studentSubmitedChallenges[challengeId][0].score,
+            studentUserName:
+              studentSubmitedChallenges[challengeId][0].hacker_username,
+          });
+        });
+
+        const totalSubmissions = studentIds.length;
+        console.log(
+          "Getting submissions ... ",
+          `${Math.round((j + 1) / totalSubmissions * 100) / 100}%`
+        );
+
+        for (let i = 0; i < submissionsIds.length; i++) {
+          getSubmission(cookie, submissionsIds[i].submissionId, contestSlug)
+            .then((submissionReposne) => submissionReposne.clone().json())
+            .then((submissionJson) => {
+              responses.push({
+                studentId: studentIds[j],
+                studentUserName: submissionsIds[i].studentUserName,
+                challenge: submissionsIds[i].challengeName,
+                score: submissionsIds[i].score,
+                code: submissionJson.model.code,
+              });
+            });
+          await snooze(6000);
+        }
+        //console.log(util.inspect(responses, false, null, true));
+        converter.json2csv(responses, (err, csv) => {
+          if (err) {
+            throw err;
+          }
+
+          // write CSV to a file
+          fs.writeFileSync("responses.csv", csv);
+        });
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+main();
